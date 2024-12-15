5d85a2b9d1091762a6454f66dd15756dd26fa2ca
Througilio Huncks <througilioh@outlook.com>
2024-12-15 15:23:27 +0100

    functies extra?

--- reclame.py
+++ reclame.py
@@ -1,6 +1,27 @@
+import statistics
+from algemene_functies import mijn_functie_2  
+
 def aanbieding_1(smaak, prijs, korting):
     korting_prijs = prijs * korting
     nieuwe_prijs = prijs - korting_prijs
-    print(f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {nieuwe_prijs} euro.")
+    return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {nieuwe_prijs} euro."
 
-aanbieding_1("aardbei", 4, 0.1)
+def inkomsten_totaal(inkomsten, btw):
+    totaal = sum(inkomsten)
+    btw_bedrag = totaal * float(btw / 100)
+    return f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag} euro btw betaald dient te worden."
+
+def laag_en_hoog(mijn_lijst):
+    return [max(mijn_lijst), min(mijn_lijst)]
+
+def gemiddelde(mijn_lijst):
+    bedrag = statistics.mean(mijn_lijst)
+    return f"De gemiddelde inkomsten deze week zijn {bedrag} euro."
+
+def meervoudig(invoer_lijst):
+    if invoer_lijst > 4 and invoer_lijst < 11:
+        return laag_en_hoog(int(invoer_lijst))
+    
+def combinatie(invoer_lijst_2): 
+    korte_lijst = meervoudig(invoer_lijst_2)
+    return mijn_functie_2(korte_lijst)
