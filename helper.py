aae072607986197076ae6ebb690d4dab9f7ab88c
Througilio Huncks <througilioh@outlook.com>
2024-12-16 00:11:47 +0100

    update

--- helper.py
+++ helper.py
@@ -10,7 +10,9 @@ def fooi_pp(bedrag, personen):
     bedrag_pp = bedrag / personen
     return f"het bedrag per persoon is {bedrag_pp} euro "
 
-b = int(input("Welk bedrag zit er in de fooienpot? "))
-p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
-
-print(fooi_pp(b,p))
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    lijn = len(tekst) * "="
+    uit.append(lijn)
+    return uit
